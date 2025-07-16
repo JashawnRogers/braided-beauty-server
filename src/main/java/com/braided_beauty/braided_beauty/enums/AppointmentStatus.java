1ab62213ef67320a1061fe9415a90ff5a2d87014
JashawnRogers <rogersjashawn20@gmail.com>
2025-07-16 00:29:32 -0700

    Created appointments table

---
+++ src/main/java/com/braided_beauty/braided_beauty/enums/AppointmentStatus.java
@@ -0,0 +1,8 @@
+package com.braided_beauty.braided_beauty.enums;
+
+public enum AppointmentStatus {
+    BOOKED,
+    CONFIRMED,
+    CANCELED,
+    COMPLETED
+}
--- src/main/java/com/braided_beauty/braided_beauty/models/Appointment.java
+++ src/main/java/com/braided_beauty/braided_beauty/models/Appointment.java
@@ -1,4 +1,57 @@
 package com.braided_beauty.braided_beauty.models;
 
+import com.braided_beauty.braided_beauty.enums.AppointmentStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "appointments")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Appointment {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "service_id")
+    private Service service;
+
+    @Column(name = "appointment_time", nullable = false, unique = true)
+    private LocalDateTime appointmentTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AppointmentStatus appointmentStatus;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
+
 }
