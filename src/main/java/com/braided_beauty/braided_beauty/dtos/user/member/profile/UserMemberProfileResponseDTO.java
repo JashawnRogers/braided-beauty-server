7d032c85a52df58b2477e583ed820b735be62a9d
JashawnRogers <rogersjashawn20@gmail.com>
2025-07-18 11:58:15 -0700

    Created user member profile response DTO

--- src/main/java/com/braided_beauty/braided_beauty/dtos/user/member/profile/UserMemberProfileResponseDTO.java
+++ src/main/java/com/braided_beauty/braided_beauty/dtos/user/member/profile/UserMemberProfileResponseDTO.java
@@ -1,22 +1,27 @@
 package com.braided_beauty.braided_beauty.dtos.user.member.profile;
 
+import com.braided_beauty.braided_beauty.dtos.appointment.AppointmentResponseDTO;
+import com.braided_beauty.braided_beauty.dtos.loyaltyRecord.LoyaltyRecordResponseDTO;
 import com.braided_beauty.braided_beauty.enums.UserType;
-import com.braided_beauty.braided_beauty.models.Appointment;
-import com.braided_beauty.braided_beauty.models.LoyaltyRecord;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.UUID;
 
 @AllArgsConstructor
 @Getter
 @Setter
-public class UserMemberProfileRequestDTO {
+public class UserMemberProfileResponseDTO {
+    @NotNull
+    private final UUID id;
     @NotNull
     private final String name;
     @NotNull
-    private final UserType userType;
-
+    private final String email;
+    private final String phoneNumber;
+    private final List<AppointmentResponseDTO> appointments;
+    private final LoyaltyRecordResponseDTO loyalty;
 }
---
+++ src/main/java/com/braided_beauty/braided_beauty/dtos/user/member/profile/UserMemberRequestDTO.java
@@ -0,0 +1,4 @@
+package com.braided_beauty.braided_beauty.dtos.user.member.profile;
+
+public class UserMemberRequestDTO {
+}
