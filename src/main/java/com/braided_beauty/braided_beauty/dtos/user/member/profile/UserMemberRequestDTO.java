3a37cef8c690efd86e9bdfefd769b96c113bab47
JashawnRogers <rogersjashawn20@gmail.com>
2025-07-18 11:58:57 -0700

    Created user member request DTO

--- src/main/java/com/braided_beauty/braided_beauty/dtos/user/member/profile/UserMemberRequestDTO.java
+++ src/main/java/com/braided_beauty/braided_beauty/dtos/user/member/profile/UserMemberRequestDTO.java
@@ -1,4 +1,18 @@
 package com.braided_beauty.braided_beauty.dtos.user.member.profile;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class UserMemberRequestDTO {
+    @NotNull
+    private final String name;
+    @NotNull
+    @Pattern(regexp = "^[0-9]{10}$", message = "Phone number must be 10 digits")
+    private final String phoneNumber;
 }
