10441af3074b9b88f65fb8072c9311def9e772ac
JashawnRogers <rogersjashawn20@gmail.com>
2025-07-18 05:48:00 -0700

    Created service request DTO

--- src/main/java/com/braided_beauty/braided_beauty/dtos/service/ServiceRequestDTO.java
+++ src/main/java/com/braided_beauty/braided_beauty/dtos/service/ServiceRequestDTO.java
@@ -1,4 +1,17 @@
 package com.braided_beauty.braided_beauty.dtos.service;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class ServiceRequestDTO {
+    private final String name;
+    private final String Description;
+    private final BigDecimal price;
+    private final Integer durationMinutes;
 }
