b99b39bc8b611d8195cd3f5d71c55e6e8457a3af
JashawnRogers <rogersjashawn20@gmail.com>
2025-07-18 05:46:30 -0700

    Created appointment response DTO

--- src/main/java/com/braided_beauty/braided_beauty/dtos/appointment/AppointmentResponseDTO.java
+++ src/main/java/com/braided_beauty/braided_beauty/dtos/appointment/AppointmentResponseDTO.java
@@ -1,4 +1,29 @@
 package com.braided_beauty.braided_beauty.dtos.appointment;
 
+import com.braided_beauty.braided_beauty.dtos.service.ServiceResponseDTO;
+import com.braided_beauty.braided_beauty.enums.AppointmentStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class AppointmentResponseDTO {
+    @NotNull
+    private final UUID id;
+    @NotNull
+    private final LocalDateTime appointmentTime;
+    @NotNull
+    private final AppointmentStatus appointmentStatus;
+    @NotNull
+    private final LocalDateTime createdAt;
+    @NotNull
+    private final ServiceResponseDTO service;
+    private final Integer pointsEarned;
+    private final LocalDateTime updatedAt;
 }
