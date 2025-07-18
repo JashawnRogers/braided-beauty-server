e7c113f6b98f3d7ab432655d7a61837db4395d8c
JashawnRogers <rogersjashawn20@gmail.com>
2025-07-18 05:51:12 -0700

    Created user login DTO

--- src/main/java/com/braided_beauty/braided_beauty/dtos/user/auth/UserLoginDTO.java
+++ src/main/java/com/braided_beauty/braided_beauty/dtos/user/auth/UserLoginDTO.java
@@ -1,4 +1,13 @@
 package com.braided_beauty.braided_beauty.dtos.user.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class UserLoginDTO {
+    private String oAuthProvider;
+    private String token;
 }
