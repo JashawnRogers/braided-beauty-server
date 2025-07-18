fafaf7296c6bd01ff1f9fc96621975c31aefc973
JashawnRogers <rogersjashawn20@gmail.com>
2025-07-18 05:48:45 -0700

    Created service response DTO

--- src/main/java/com/braided_beauty/braided_beauty/dtos/service/ServiceResponseDTO.java
+++ src/main/java/com/braided_beauty/braided_beauty/dtos/service/ServiceResponseDTO.java
@@ -1,4 +1,20 @@
 package com.braided_beauty.braided_beauty.dtos.service;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class ServiceResponseDTO {
+    private final UUID id;
+    private final String name;
+    private final String description;
+    private final BigDecimal price;
+    private final Integer durationMinutes;
 }
