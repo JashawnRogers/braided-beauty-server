f217560582c2c824a18e5e65170f6f02b4c70ad3
JashawnRogers <rogersjashawn20@gmail.com>
2025-07-18 05:47:18 -0700

    Created loyalty record response DTO

--- src/main/java/com/braided_beauty/braided_beauty/dtos/loyaltyRecord/LoyaltyRecordResponseDTO.java
+++ src/main/java/com/braided_beauty/braided_beauty/dtos/loyaltyRecord/LoyaltyRecordResponseDTO.java
@@ -1,4 +1,13 @@
 package com.braided_beauty.braided_beauty.dtos.loyaltyRecord;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class LoyaltyRecordResponseDTO {
+    private final Integer points;
+    private final Integer redeemedPoints;
 }
