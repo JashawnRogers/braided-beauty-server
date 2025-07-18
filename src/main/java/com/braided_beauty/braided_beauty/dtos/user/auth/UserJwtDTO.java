9109f37a739b264bc7b9c2da5ce9dc27394bfc8e
JashawnRogers <rogersjashawn20@gmail.com>
2025-07-18 05:50:17 -0700

    Created user JWT DTO (Should be sent with every authenticated request)

--- src/main/java/com/braided_beauty/braided_beauty/dtos/user/auth/UserJwtDTO.java
+++ src/main/java/com/braided_beauty/braided_beauty/dtos/user/auth/UserJwtDTO.java
@@ -1,4 +1,12 @@
 package com.braided_beauty.braided_beauty.dtos.user.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class UserJwtDTO {
+    private final String token;
 }
