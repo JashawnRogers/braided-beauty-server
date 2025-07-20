588c141e774c5cc3f250214316104b5cbc337f27
JashawnRogers <rogersjashawn20@gmail.com>
2025-07-20 16:38:17 -0700

    Created DTO mappers for all DTOs related to business hours

--- src/main/java/com/braided_beauty/braided_beauty/dtos/shared/BusinessHoursResponseDTO.java
+++ src/main/java/com/braided_beauty/braided_beauty/dtos/shared/BusinessHoursResponseDTO.java
@@ -1,6 +1,7 @@
 package com.braided_beauty.braided_beauty.dtos.shared;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +12,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class BusinessHoursResponseDTO {
    private final UUID id;
    private final DayOfWeek dayOfWeek;
--- src/main/java/com/braided_beauty/braided_beauty/mappers/shared/BusinessHoursDtoMapper.java
+++ src/main/java/com/braided_beauty/braided_beauty/mappers/shared/BusinessHoursDtoMapper.java
@@ -1,4 +1,29 @@
-package com.braided_beauty.braided_beauty.mappers.businessHours;
+package com.braided_beauty.braided_beauty.mappers.shared;
 
+import com.braided_beauty.braided_beauty.dtos.shared.BusinessHoursRequestDTO;
+import com.braided_beauty.braided_beauty.dtos.shared.BusinessHoursResponseDTO;
+import com.braided_beauty.braided_beauty.models.BusinessHours;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BusinessHoursDtoMapper {
+
+    public BusinessHours toEntity(BusinessHoursRequestDTO dto){
+        return BusinessHours.builder()
+                .dayOfWeek(dto.getDayOfWeek())
+                .openTime(dto.getOpenTime())
+                .closeTime(dto.getCloseTime())
+                .isClosed(dto.isClosed())
+                .build();
+    }
+
+    public BusinessHoursResponseDTO toDto(BusinessHours dto){
+        return BusinessHoursResponseDTO.builder()
+                .id(dto.getId())
+                .dayOfWeek(dto.getDayOfWeek())
+                .openTime(dto.getOpenTime())
+                .closeTime(dto.getCloseTime())
+                .isClosed(dto.isClosed())
+                .build();
+    }
 }
