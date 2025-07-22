c3949022621d27dc423f4364fd79376e39aa0f3b
JashawnRogers <rogersjashawn20@gmail.com>
2025-07-21 23:17:50 -0700

    Created payments controller

---
+++ src/main/java/com/braided_beauty/braided_beauty/controllers/PaymentController.java
@@ -0,0 +1,27 @@
+package com.braided_beauty.braided_beauty.controllers;
+
+import com.braided_beauty.braided_beauty.dtos.payment.PaymentIntentRequestDTO;
+import com.braided_beauty.braided_beauty.dtos.payment.PaymentIntentResponseDTO;
+import com.braided_beauty.braided_beauty.services.PaymentService;
+import com.stripe.exception.StripeException;
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/payments")
+@AllArgsConstructor
+public class PaymentController {
+    private final PaymentService paymentService;
+
+    @PostMapping("/create-intent")
+    public ResponseEntity<PaymentIntentResponseDTO> createIntent(@RequestBody PaymentIntentRequestDTO dto){
+        try{
+            PaymentIntentResponseDTO response = paymentService.createPaymentIntent(dto);
+            return ResponseEntity.ok(response);
+        } catch (StripeException e) {
+            return ResponseEntity.status(500).build();
+        }
+    }
+}
