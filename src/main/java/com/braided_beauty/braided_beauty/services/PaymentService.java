4cd43e16f929705e258f77292bacdc351c310bc9
JashawnRogers <rogersjashawn20@gmail.com>
2025-07-21 23:08:41 -0700

    Created payment service

---
+++ src/main/java/com/braided_beauty/braided_beauty/services/PaymentService.java
@@ -0,0 +1,35 @@
+package com.braided_beauty.braided_beauty.services;
+
+import com.braided_beauty.braided_beauty.dtos.payment.PaymentIntentRequestDTO;
+import com.braided_beauty.braided_beauty.dtos.payment.PaymentIntentResponseDTO;
+import com.braided_beauty.braided_beauty.mappers.payment.PaymentDtoMapper;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import lombok.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@Service
+public class PaymentService {
+    @Autowired
+    private final PaymentDtoMapper paymentDtoMapper;
+    private String stripeApiKey;
+
+    @PostConstruct
+    public void init(){
+        Stripe.apiKey = stripeApiKey;
+    }
+
+    public PaymentIntentResponseDTO createPaymentIntent(PaymentIntentRequestDTO dto) throws StripeException {
+        PaymentIntentCreateParams createParams = paymentDtoMapper.toStripeParams(dto);
+        PaymentIntent paymentIntent = PaymentIntent.create(createParams);
+        return paymentDtoMapper.toDto(paymentIntent);
+    }
+}
