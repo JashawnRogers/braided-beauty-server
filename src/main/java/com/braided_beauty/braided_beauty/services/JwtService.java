b61d905d2f5039247b63d1f2b8b58ea4850efd2c
JashawnRogers <rogersjashawn20@gmail.com>
2025-08-11 14:46:10 -0700

    Created JWT service class

---
+++ src/main/java/com/braided_beauty/braided_beauty/services/JwtService.java
@@ -0,0 +1,37 @@
+package com.braided_beauty.braided_beauty.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class JwtService {
+
+    private final JwtEncoder jwtEncoder;
+
+    public String generateAccessToken(Authentication authentication) {
+        var now = Instant.now();
+
+        var scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+
+        var claims = JwtClaimsSet.builder()
+                .issuer("braided-beauty")
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(30 * 60))
+                .subject(authentication.getName())
+                .claim("scope", scope)
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+}
