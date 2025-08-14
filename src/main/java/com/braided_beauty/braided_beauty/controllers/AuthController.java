858af76b130fa18ac4b0e5fcb762c29345c2461d
JashawnRogers <rogersjashawn20@gmail.com>
2025-08-13 21:49:26 -0700

    Created register route for users

--- src/main/java/com/braided_beauty/braided_beauty/controllers/AuthController.java
+++ src/main/java/com/braided_beauty/braided_beauty/controllers/AuthController.java
@@ -1,8 +1,11 @@
 package com.braided_beauty.braided_beauty.controllers;
 
 import com.braided_beauty.braided_beauty.dtos.user.auth.LoginRequestDTO;
+import com.braided_beauty.braided_beauty.dtos.user.auth.UserRegistrationDTO;
+import com.braided_beauty.braided_beauty.services.AuthService;
 import com.braided_beauty.braided_beauty.services.JwtService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -21,6 +24,7 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final AuthService authService;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequestDTO dto) {
@@ -30,5 +34,17 @@ public class AuthController {
 
         String token = jwtService.generateAccessToken(auth);
         return ResponseEntity.ok(Map.of("accessToken", token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody UserRegistrationDTO dto) {
+        authService.register(dto);
+
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword())
+        );
+
+        String token = jwtService.generateAccessToken(auth);
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("accessToken", token));
     }
 }
